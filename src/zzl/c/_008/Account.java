ccbe71e6b14f3fd98107c632d22f2a1adc13d143
张泽立 <853089986@qq.com>
2018-02-25 14:08:38 +0800

    dirtyRead

---
+++ src/zzl/c/_008/Account.java
@@ -0,0 +1,47 @@
+/**
+ * 对业务写方法加锁
+ * 对业务读方法不加锁
+ * 容易产生脏读问题（dirtyRead）
+ * @author zhangzeli
+ */
+package zzl.c._008;
+
+import java.util.concurrent.TimeUnit;
+
+public class Account{
+	String name;
+	double balance;
+	
+	public synchronized void set(String name,double balance) {
+		this.name=name;
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		this.balance=balance;
+	}
+	
+	public double getBalance(String name) {
+		return this.balance;
+	}
+	
+	public static void main(String[] args) {
+		Account a = new Account();
+		 new Thread(()->a.set("zhangsan", 100.0),"a1").start();
+		 try {
+			TimeUnit.SECONDS.sleep(1);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		 System.out.println(a.getBalance("zhangsan"));
+		 try {
+				TimeUnit.SECONDS.sleep(2);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		 System.out.println(a.getBalance("zhangsan"));
+	
+	}
+
+}
